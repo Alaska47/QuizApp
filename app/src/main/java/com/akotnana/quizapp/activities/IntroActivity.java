7ed35b5a721cee90ca9565b2e29bad335cf45663
Aneesh Kotnana <aneesh.kotnana@gmail.com>
2018-02-06 21:47:14 -0500

    Upload initial project

---
+++ app/src/main/java/com/akotnana/quizapp/activities/IntroActivity.java
@@ -0,0 +1,25 @@
+package com.akotnana.quizapp.activities;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.akotnana.quizapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/akotnana/quizapp/utils/FontManager.java
@@ -0,0 +1,8 @@
+package com.akotnana.quizapp.utils;
+
+/**
+ * Created by anees on 2/6/2018.
+ */
+
+public class FontManager {
+}
