85de830afc29b94bcfe32a0ff5bbe9478e055f60
Aneesh Kotnana <aneesh.kotnana@gmail.com>
2018-02-16 14:45:53 -0500

    Add activity to manifest

---
+++ app/src/main/java/com/akotnana/quizapp/activities/QuizSelectionActivity.java
@@ -0,0 +1,16 @@
+package com.akotnana.quizapp.activities;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.akotnana.quizapp.R;
+
+public class QuizSelectionActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_selection);
+    }
+}
